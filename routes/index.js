600481788fe67b1af8d1154dc41b1d460df947d6
OmieWalls <Omie.Walls@daugherty.com>
2018-09-17 16:05:04 -0400

    Added Datastore integration

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,23 @@ router.get('/', function(req, res, next) {
     // Imports the Google Cloud client library
     const PubSub = require(`@google-cloud/pubsub`);
 
+    // Imports the Google Cloud client library
+    const Datastore = require('@google-cloud/datastore');
+
     // Your Google Cloud Platform project ID
     const projectId = 'test-mock-service';
+
+
 
 // Initiating a client
     const pubsub = new PubSub({
         projectId: projectId,
         keyFilename: process.env.HOME+'/Projects/Secrets/test-mock-service-new.json'
+    });
+
+// Creates a client
+    const datastore = new Datastore({
+        projectId: projectId,
     });
 
     /**
@@ -28,11 +38,49 @@ const timeout = 60;
     let messageCount = 0;
     const messageHandler = message => {
         console.log(`Received message ${message.id}:`);
-        console.log(`\tData: ${message.data}`);
-        console.log(`\tAttributes: ${message.attributes}`);
+        console.log(`\tData: ${message.data.toString('UTF8')}`);
+        console.log(`\tAttributes: ${JSON.stringify(message.attributes)}`);
         messageCount += 1;
+        //TODO: Store data
+        const data = message.data.toString('UTF8');
+        const json = JSON.parse(data);
+// The kind for the new entity
+        const kind = 'Test';
+// The name/ID for the new entity
+        const name = json.Name;
+// The Cloud Datastore key for the new entity
+        const usCompaniesKey = datastore.key([kind, name]);
 
-        // "Ack" (acknowledge receipt of) the message
+        const usCompaniesData = {
+            Action: json.Action,
+            City: json.City,
+            Company_ID: json.Company_ID,
+            Country: json.Country,
+            Created_On: json.Created_On,
+            Description: json.Description,
+            Is_Active: json.Is_Active,
+            Name: json.Name,
+            Slogan: json.Slogan,
+            State: json.State
+        };
+
+// Prepares the new entity
+        const usCompanies = {
+            key: usCompaniesKey,
+            data: usCompaniesData,
+        };
+
+// Saves the entity
+        datastore
+            .upsert(usCompanies)
+            .then(() => {
+                console.log(`Saved ${usCompanies.key.name}: ${usCompanies.data}`);
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+
+        // Acknowledge receipt of the message
         message.ack();
     };
 
